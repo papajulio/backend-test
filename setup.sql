f2fef69698ce31d3541877f07fda30397297a1f2
Julio García <papajulio21@gmail.com>
2017-07-02 16:11:18 +0200

    Make unit tests work

--- setup.sql
+++ setup.sql
@@ -12,3 +12,5 @@ drop role if exists backend_test;
 create database backend_test;
 create role backend_test with login password 'backend_test';
 grant all privileges on database backend_test to backend_test;
+ALTER DATABASE backend_test OWNER TO backend_test;
+ALTER USER backend_test CREATEDB;
