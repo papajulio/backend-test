9941248e646bac69877a3cfa856aa033da86daf8
Julio García <papajulio21@gmail.com>
2017-07-04 19:32:20 +0200

    Challenge 1 list of users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+      @users = User.joins("LEFT JOIN groups_users ON user_id = users.id LEFT JOIN groups ON group_id = groups.id")
+      @users = @users.select("users.name, users.created_at, count(groups.name) AS num_groups, string_agg(groups.name, ', ') AS groups_names")
+      @users = @users.group("users.name, users.created_at")
   end
 end
