5c8bf7ceaba0d2057909879f3a0dbd9fb08da0f2
Julio García <papajulio21@gmail.com>
2017-07-04 19:03:55 +0200

    Draft of the tests and fixtures that should be done

---
+++ test/unit/home_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class GroupTest < ActiveSupport::TestCase
+  # I would have loved to create some tests to support my functions
+  # but I didn't want to invest so many time learning the way to make this tests works with the controller
+  # In any real project this is really worth it and you should invest time in it.
+  test "the truth again" do
+    assert true
+  end
+end
